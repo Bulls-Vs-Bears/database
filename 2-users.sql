9ad7e4eb1ecb41e070195f4a03fb2c4ef333b6f3
Darren Chan <darren@darchan0319@gmail.com>
2020-07-21 21:26:49 -0700

    debugging role access

--- 2-users.sql
+++ 2-users.sql
@@ -1,17 +1,17 @@
 -- Create reader group
-CREATE ROLE readaccess;
+CREATE ROLE readaccess LOGIN;
 GRANT USAGE ON SCHEMA bvb_accounts TO readaccess;
 ALTER DEFAULT PRIVILEGES IN SCHEMA bvb_accounts GRANT SELECT ON TABLES TO readaccess;
 -- GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public to www;
 -- GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA bvb_accounts to readaccess;
 -- Create Writer Group
-CREATE ROLE writeaccess;
+CREATE ROLE writeaccess LOGIN;
 GRANT USAGE ON SCHEMA bvb_accounts TO writeaccess;
 ALTER DEFAULT PRIVILEGES IN SCHEMA bvb_accounts GRANT SELECT, INSERT, UPDATE ON TABLES TO writeaccess;
 -- GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA bvb_accounts to writeaccess;
 
 -- Create Deletion Group
-CREATE ROLE deleteaccess;
+CREATE ROLE deleteaccess LOGIN;
 GRANT USAGE ON SCHEMA bvb_accounts TO deleteaccess;
 ALTER DEFAULT PRIVILEGES IN SCHEMA bvb_accounts GRANT SELECT, DELETE ON TABLES TO deleteaccess;
 -- GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA bvb_accounts to deleteaccess;
