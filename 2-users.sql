564eebc5f9bf40ed1fdeb89e8af287780c2cccb5
jontkoh <54458111+jontkoh@users.noreply.github.com>
2020-07-27 21:40:39 -0700

    Revert "User table update"

--- 2-users.sql
+++ 2-users.sql
@@ -1,17 +1,17 @@
 -- Create reader group
-CREATE ROLE readaccess LOGIN;
+CREATE ROLE readaccess;
 GRANT USAGE ON SCHEMA bvb_accounts TO readaccess;
 ALTER DEFAULT PRIVILEGES IN SCHEMA bvb_accounts GRANT SELECT ON TABLES TO readaccess;
 
 -- Create Writer Group
-CREATE ROLE writeaccess LOGIN;
+CREATE ROLE writeaccess;
 GRANT USAGE ON SCHEMA bvb_accounts TO writeaccess;
-ALTER DEFAULT PRIVILEGES IN SCHEMA bvb_accounts GRANT SELECT, INSERT, UPDATE ON TABLES TO writeaccess;
+ALTER DEFAULT PRIVILEGES IN SCHEMA bvb_accounts GRANT SELECT, INSERT, UPDATE ON TABLES TO readaccess;
 
 -- Create Deletion Group
-CREATE ROLE deleteaccess LOGIN;
+CREATE ROLE deleteaccess;
 GRANT USAGE ON SCHEMA bvb_accounts TO deleteaccess;
-ALTER DEFAULT PRIVILEGES IN SCHEMA bvb_accounts GRANT SELECT, DELETE ON TABLES TO deleteaccess;
+ALTER DEFAULT PRIVILEGES IN SCHEMA bvb_accounts GRANT SELECT, DELETE ON TABLES TO readaccess;
 
 -- bvb reader
 DROP USER IF EXISTS bvb_reader;
